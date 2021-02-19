c23258717a784c99b580375a1c3c7090d72a754f
Felixpointexe <76926119+Felixpointexe@users.noreply.github.com>
2021-02-19 14:01:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const fetch= require('node-fetch');
 
-const replies = [
-    'Pong Dong',
-    'Gesundheit!',
-    'ne da, da fehlen mir die Worte!'
 
-]
+
+
 
  
 
@@ -23,61 +19,25 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    let tokens =  message.content.split(" ");
+    client.on('message', message => {	client.on('message', message => {
 
-    if(tokens[0] === '/ping') {
+
+    if (message.content === 'ping') {	    let tokens = msg.content.split(" ");
+
+
+       message.reply('pong');	    if(tokens[0] === '/ping') {
         const r = Math.floor(Math.random() * replies.length);
         message.reply(replies[r]);
 
-    }
 
-    if(tokens[0] === '/gif'){
-
-        let keywords = 'nothing';
-        if(tokens.length > 1){
-            keywords = tokens.slice(1,tokens.length).join(" ");
-
-        }
-        
-        let url = `https://g.tenor.com/v1/search?q=${keywords}&key=1O4OB5Z2JAMM&limit=8`;
-        let response = await fetch(url);
-        let json = await response.json();
-        console.log(json);
-
-        let index = Math.floor(Math.random() * json.results.length);
-        message.channel.send(json.results[index].url);
+       }	    }
+    if (message.content === 'hs') {	
 
 
-    }
-    if(tokens[0] === '/vote'){
-        let keywords = 'nothing';
-        if(tokens.length > 1){
-            keywords = tokens.slice(1,tokens.length).join(" ");
-        }
-
-        const embed = new Discord.MessageEmbed()
-      
-        .setTitle('vote for:')
-        .setColor(0x00ff00)
-        .setDescription(keywords);
-
-        message.channel.send(embed).then(function(sentMessage) {
-            sentMessage.react('👍').then(() => sentMessage.react('👎')).catch(() => console.error('emoji failed to react.'));
-        });
+       message.reply('selber');	    if(tokens[0] === '/gif'){
 
 
-    }
-    if(tokens[0] === '/help'){
-
-        const embed = new Discord.MessageEmbed()
-      
-        .setTitle('here is some help')
-        .setColor(0xff0000)
-        .setDescription('/ping ; /gif [your search] ; /vote [your voting theme]');
-
-        message.reply(embed);
-
-    }
+       }
 
 
   
