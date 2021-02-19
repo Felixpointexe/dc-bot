c6d2f6c0a7354bd8b2867b5b084cdd7c8e15956c
Felixpointexe <76926119+Felixpointexe@users.noreply.github.com>
2021-02-19 17:29:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ client.on('message', message => {
         if(tokens.length > 1){
             keywords = tokens.slice(1,tokens.length).join(" ");
         }
+  
+        const embed = new Discord.MessageEmbed()
+      
+        .setTitle('vote for:')
+        .setColor(0x00ff00)
+        .setDescription(keywords);
+  
+  
         message.react('👍');
         message.react('👎');
   
