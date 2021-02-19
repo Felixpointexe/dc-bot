ed7d4bc792e23ba43137b9c4fe8bb52976425383
Felixpointexe <76926119+Felixpointexe@users.noreply.github.com>
2021-02-19 14:12:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('message', message => {
     let tokens = message.content.split(" ");
 
     if(tokens[0] === 'ping') {
-        const r = Math.floor(Math.random() * replies.length);
+        
         message.reply('dein ping ist mir pong');
 
     }
