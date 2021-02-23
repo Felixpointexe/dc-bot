1e0eddaa52cce60ddb5a078d038e82da6aba8e5c
Felixpointexe <76926119+Felixpointexe@users.noreply.github.com>
2021-02-23 11:46:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ async function gotMessage(message){
     if(tokens[0] === '/help'){
         message.reply({embed: {
           "url": "https://discordapp.com",
-    "color": 3dd2f2,
+    "color": 2815231,
     
     "footer": {
       "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
