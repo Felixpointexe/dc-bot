d1ef483fd278903b033a27b55d6055834dfc1549
Felixpointexe <76926119+Felixpointexe@users.noreply.github.com>
2021-02-25 14:03:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -239,23 +239,20 @@ async function gotMessage(message){
     if(tokens[0] === '/bastard'){
     {
   
-       message.channel.send({  "embed": {
-       "title": "Der Bastard-award",
-       "description": "findet, dass  ein Bastard ist. \n Was denkt dir darüber? Stimmt doch mal ab",
-       "color": 13632027,
-       "timestamp": "2021-02-25T12:22:15.923Z",
-     
-
-    
-    "image": {
-      "url": "https://assets.stickpng.com/images/580b585b2edbce24c47b2af7.png"
-    },
-    "author": {
-      "name": "author name",
-      "url": "https://discordapp.com",
-      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-    }
-    }});
+       message.channel.send({embed: {
+          "title": "Der Bastard-award",
+          "description": "findet, dass  ein Bastard ist. \n Was denkt dir darüber? Stimmt doch mal ab",
+          "color": 13632027,
+          "timestamp": "2021-02-25T12:22:15.923Z",
+          "image": {
+             "url": "https://assets.stickpng.com/images/580b585b2edbce24c47b2af7.png"
+           },
+           "author": {
+              "name": "author name",
+              "url": "https://discordapp.com",
+              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+           }
+        }});
         
     }
     
