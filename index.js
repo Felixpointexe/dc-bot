33c5ffea4fbdf27fd7e48a09a703a6fbcafcb7d3
Felixpointexe <76926119+Felixpointexe@users.noreply.github.com>
2021-02-25 15:29:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on('message', gotMessage );
 async function gotMessage(message){ 
     let tokens = message.content.split(" ");
     
-    if(message.channel.id != '813345162410459146' && message.channel.id != '812003291436154942'){
+    if(message.channel.id != '813345162410459146' && message.channel.id != '812003291436154942' && message.channel.id !=  '814503926379446336'){
         return;
     }
 
